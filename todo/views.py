19fdfd039361ed552815c1e6b4de4f3b0e1f26e8
Yeshwanth <yeshd0205@gmail.com>
2023-10-28 04:37:51 +0530

    todo app created

--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,get_object_or_404
 from django.http import HttpResponse
 from .models import TodoItem
 
@@ -7,6 +7,29 @@ from .models import TodoItem
 def addTask(request):
    task=request.POST['task']
    TodoItem.objects.create(title=task,)
+   return redirect('home')
 
+def mark_as_done(request,pk):
+   task=get_object_or_404(TodoItem,pk=pk)
+   task.completed=True
+   task.save()
+   return redirect('home')
 
+def edit_task(request, pk):
+    get_task = get_object_or_404(TodoItem, pk=pk)
+    if request.method == 'POST':
+        new_task = request.POST['task']
+        get_task.task = new_task
+        get_task.save()
+        return redirect('home')
+    else:
+        context = {
+            'get_task': get_task,
+        }
+        return render(request, 'edit_task.html', context)
+   
+
+def delete_task(request,pk):
+   task=get_object_or_404(TodoItem,pk=pk)
+   task.delete()
    return redirect('home')
--- todoapp/views.py
+++ todoapp/views.py
@@ -3,7 +3,9 @@ from todo.models import TodoItem
 
 def home(request):
     tasks= TodoItem.objects.filter(completed=False).order_by('-created_at')
+    completed_tasks = TodoItem.objects.filter(completed=True)
     context={
         'tasks':tasks,
+        'completed_tasks':completed_tasks,
     }
     return render(request,'home.html',context)
